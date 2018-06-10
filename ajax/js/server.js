1ca682f0047abf65c468dc1273eabe5444f51ba3
Romulo Lacerda <rlcrweb@gmail.com>
2018-06-10 20:17:36 -0300

    new studies

--- ajax/js/server.js
+++ ajax/js/server.js
@@ -6,5 +6,39 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function(req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err) {
+            return res.end('Erro')
+        }
+        res.end('Concluido')
+    })
+})
+
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,  
+        id: 1
+    })
+})
+
+app.get('/parOuImpar', (req, res) => {
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
 app.get('/teste', (req, res) => res.send('Ok'))
 app.listen(8081, () => console.log('Executando...'))
