164fad2097fee34c0362f920e75ba7b7b5433394
Romulo Lacerda <rlcrweb@gmail.com>
2018-06-25 13:26:21 -0300

    Webpack and Gulp

---
+++ gulp/basico/gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp')
+gulp.task('default', () => {
+    gulp.start('copiar', 'fim')
+})
+
+gulp.task('copiar', ['antes1', 'antes2'], () => {
+    gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+        .pipe(gulp.dest('pastaB'))
+})
+
+gulp.task('antes1', function() {
+    console.log('Antes 1')
+})
+
+gulp.task('antes2', () => {
+    console.log('antes 2')
+})
+
+gulp.task('fim', () => {
+    console.log('fim')
+})
---
+++ gulp/spa/gulpTasks/server.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp')
+const watch = require('gulp-watch')
+const webserver = require('gulp-webserver')
+
+
+gulp.task('monitorar', () => {
+    watch('src/**/*.html', () => gulp.start('app.html'))
+    watch('src/**/*.scss', () => gulp.start('app.css'))
+    watch('src/**/*.js', () => gulp.start('app.js'))
+    watch('src/assets/imgs/**/*.*', () => gulp.start('app.imgs'))
+})
+
+gulp.task('server', ['monitorar'], () => {
+    return gulp.src('build').pipe(webserver({
+        livereload: true,
+        port: 8080,
+        open: true
+    }))
+})
