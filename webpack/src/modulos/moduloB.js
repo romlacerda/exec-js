da93ccef6fcd900f358616ce88571bfc6e858146
Rômulo de Lacerda Correa Rimulo <romulo.rimulo@radixeng.com.br>
2018-06-26 17:55:53 -0300

    Starting new project

---
+++ webpack/src/modulos/moduloB.js
@@ -0,0 +1,5 @@
+module.exports = {
+    saudacoes() {
+        return 'Olá sou o Módulo B.'
+    }
+}
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,46 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/principal.js'
+    mode: modoDev ? 'development' : 'production',
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    },
+    devServer: {
+        contentBase: "./public",
+        port: 9000
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsWebpackPlugin({})
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "style.css"
+        })
+    ],
+    module: {
+         rules: [{
+             test: /\.s?[ac]ss$/,
+             use: [
+                 MiniCssExtractPlugin.loader,
+                 //'style-loader', // Adiciona CSS a dom injeanto a tag style
+                 'css-loader', // Interpreta @import, url()
+                 'sass-loader'
+             ]
+         }, {
+             test: /\.(´mg|svg|jpg|gif)$/,
+             use: ['file-loader']
+         }]
+    }
 }
